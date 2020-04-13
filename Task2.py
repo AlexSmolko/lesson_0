35c001ab89a829904c8badf37ee9c9c1519e63f8
Aresandro <34611935+Aresnadro@users.noreply.github.com>
2020-04-13 14:41:54 +0300

    Lesson_Zero

---
+++ Task2.py
@@ -0,0 +1,8 @@
+# { < Ferenzvarosh |^| Bronbu' |^| Malme |^| Dinamo > } - { ⚽ ️final of the 4th ⚽ }
+print('{ < Ferenzvarosh', 'Bronbu\'', 'Malme', 'Dinamo', sep=' |^| ', end=' > } - { ⚽️ final of the 4th ⚽ }\n')
+
+# Oxxxymiron vs Eminem VS Ramones vs Struts Battle of the age
+print('\n - & - Oxxxymiron vs Eminem', 'Ramones vs Struts', sep=' VS ', end="- & - \n \t \t \t \tBattle of the age \n")
+#  	 	     	 	- I 🎸 music -
+#  	 	     	 	- I 🏆 sport -
+print('\n \t \t     \t \t- I 🎸 music - \n \t \t     \t \t- I 🏆 sport -')
